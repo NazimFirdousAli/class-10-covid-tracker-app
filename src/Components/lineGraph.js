b1cc060a326cb164ad297e6f84808ec888608d02
NazimFirdousAli <51528955+NazimFirdousAli@users.noreply.github.com>
2020-12-16 00:37:04 +0500

    Final Deployment

--- src/Components/lineGraph.js
+++ src/Components/lineGraph.js
@@ -1,54 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Bar } from "react-chartjs-2";
 
+const useStyles = makeStyles((theme) => ({
+  // container: {
+  //   width: "80%",
+  //   marginTop: 30,
+  //   display: "flex",
+  //   justifyContent: "center",
+  //   alignContent: "center",
+  //   alignItems: "center",
+    
+  // },
+  "& > *": {
+    paddingTop: 20,
+    margin: theme.spacing(5),
+    width: theme.spacing(30),
+    height: theme.spacing(15),
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+}));
 
-function LineGraph() {
-
-  const data = {
-    labels: [],
-    datasets: [
-      {
-        label: 'My First dataset',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: []
-      }
-    ]
-  };
-  let covid = [];
-
+function LineGraph(){
   const [graphData, setGraphData] = useState({});
   useEffect(() => {
     async function getData() {
       const response = await fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations')
       let data = await response.json();
-      setGraphData(data.locations);
+      setGraphData(data);
     }
     getData();
   }, [])
+  return(
+    console.log(graphData.latest.confirmed),
+    <Bar
+      data={{
+        labels: ["Infected", "Recovered", "Deaths"],
+        datasets: [
+          {
+            label: "People",
+            backgroundColor: [
+              "rgba(0,0,250,0.5)",
+              "rgba(0,250,0,0.5)",
+              "rgba(250,0,0,0.5)",
+            ],
+            data: [graphData.latest.confirmed, graphData.latest.recovered, graphData.latest.deaths],
+          },
+        ],
+      }}
+      options={{
+        lagend: { display: false },
+        title: { display: true, text: `Current Stats from Global Data` },
+      }}
+    />
+  )
 
-  return (
-    <div>
-      <h2>Bar Example (custom size)</h2>
-      {Object.values(graphData).map((value, index) => {
-        return (
-          console.log(value.country),
-          console.log(value.latest.confirmed)
-        )
-      })}
-      <Bar
-        data={data}
-        width={100}
-        height={50}
-        options={{
-          maintainAspectRatio: false
-        }}
-      />
-    </div>
-  );
 }
-
 export default LineGraph;
